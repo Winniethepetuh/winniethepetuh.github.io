ca615f333f68f8160626c9f53368810886077d56
Winniethepetuh <55308967+Winniethepetuh@users.noreply.github.com>
2019-10-24 15:35:30 +0600

    Add files via upload

--- Baby/order.php
+++ Baby/order.php
@@ -1,69 +1,50 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ru-ru" lang="ru-ru" dir="ltr">
-<head>
-
- <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-	<title>Поздравляем! Ваш заказ принят!</title>
-	<?php
-	
-    // Генерируем сообщение
-	
-       
-		 
-       
-			$message.= iconv("UTF-8", "UTF-8", "Имя: ")."$name \n";
-			$message.= iconv("UTF-8", "UTF-8", "Телефон: ")."$phone \n";
-		
-		
-      
-
-    // $to -- ЗДЕСЬ ВВОДИМ ПОЧТОВЫЕ АДРЕСА, НА КОТОРЫЕ ОТПРАВЛЯТЬ РЕЗУЛЬТАТ.
-      $to = "testmail@gmail.com"; // если ящиков несколько - через запятую и пробел : "me@localhost.com, me@localhost.com"
-
-    // $subject -- Тема письма
-        $subject = "Cosmo plus заявка";
-
-        $headers  = "MIME-Version: 1.0";
-        $headers .= "Content-type: text/html; charset=windows-1251" . "\r\n";
-    // После From: -- вводим имя и адрес отправителя.
-        $headers .= "From:".iconv("windows-1251", "UTF-8", "zakaz")."<zaka>" . "\r\n";
-
-        // Отправляем
-        mail($to, '=?utf-8?B?' . base64_encode($subject). '?=', $message, $headers);
-    
-	
+<?php
+/* Осуществляем проверку вводимых данных и их защиту от враждебных 
+скриптов */
+$your_name = htmlspecialchars($_POST["your_name"]);
+$phone = htmlspecialchars($_POST["phone"]);
+/* Устанавливаем e-mail адресата */
+$myemail = "elnaumova@ukr.net";
+/* Проверяем заполнены ли обязательные поля ввода, используя check_input 
+функцию */
+$your_name = check_input($_POST["your_name"], "Введите ваше имя!");
+$phone = check_input($_POST["phone"], "Укажите ваш номер телефона!");
+/* Создаем новую переменную, присвоив ей значение */
+$message_to_myemail = "Здравствуйте! 
+C вами скоро свяжется наш сотрудник! 
+Имя отправителя: $your_name 
+Телефон отправителя: $phone
+Конец";
+/* Отправляем сообщение, используя mail() функцию */f
+$from  = "From: $yourname <$email> \r\n Reply-To: $email \r\n"; 
+mail($myemail, $tema, $message_to_myemail, $from);
 ?>
-	
-	
-	
-<link rel="stylesheet" type="text/css" href="order_files/index.css" media="all">
-
-
-</head>
+<p>Ваше сообщение было успешно отправлено!</p>
+<p>На <a href="index.html">Главную >>></a></p>
+<?php
+/* Если при заполнении формы были допущены ошибки сработает 
+следующий код: */
+function check_input($data, $problem = "")
+{
+$data = trim($data);
+$data = stripslashes($data);
+$data = htmlspecialchars($data);
+if ($problem && strlen($data) == 0)
+{
+show_error($problem);
+}
+return $data;
+}
+function show_error($myError)
+{
+?>
+<html>
 <body>
-
-	<div class="wrap_block_success">
-		<div class="block_success">					
-			<h2>Поздравляем! Ваш заказ принят!</h2>
-			
-<a href="more.php" class="url_more_info" target="_blank">Нажмите здесь для получения более подробной информации о заказе</a>
-			<p class="success">В ближайшее время с вами свяжется оператор для подтверждения заказа. Пожалуйста, включите ваш контактный телефон.</p>
-			
-			<h3>Пожалуйста, проверьте правильность введенной вами информации</h3>
-			<div class="wrap_list_info">
-				<ul class="list_info">
-					 					 <li><span>Ваш телефон: </span><?php echo $phone; ?></li> 				
-					 </ul>
-			</div>
-				<p class="fail">Если вы ошиблись при заполнени формы, то, пожалуйста, <a href="index.php">заполните заявку еще раз</a></p>
-				<hr>
-				<br>
-				<div class="clear"></div>
-		
-			</form>
-			
-		</div>
-
-	</div>
+<p>Пожалуйста исправьте следующую ошибку:</p>
+<?php echo $myError; ?>
 </body>
 </html>
+<?php
+exit();
+}
+?>
