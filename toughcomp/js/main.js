42fcd8cdb94861428c372bf560d5e45d9a662e08
Winniethepetuh <55308967+Winniethepetuh@users.noreply.github.com>
2019-11-19 19:41:51 +0600

    Add files via upload

--- toughcomp/js/main.js
+++ toughcomp/js/main.js
@@ -1,8 +1,11 @@
 $(document).ready(function(){
     $('.slider').bxSlider({
         minSlides: 1,
-		maxSlides: 3,
-        slideWidth: 258,
+		maxSlides: 5,
+        slideWidth: 180,
+        auto: true,
+        pause: 3000,
+        controls: true,
         pager: ($(".bxslider2>.wrap").length > 1) ? true: false,
     });
   });
