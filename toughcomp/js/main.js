ec31244f442180be0b05e0d130110d7dc5ce362a
Winniethepetuh <55308967+Winniethepetuh@users.noreply.github.com>
2019-11-19 17:54:11 +0600

    Add files via upload

---
+++ toughcomp/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.slider').bxSlider({
+        minSlides: 1,
+		maxSlides: 3,
+        slideWidth: 258,
+        pager: ($(".bxslider2>.wrap").length > 1) ? true: false,
+    });
+  });
+
+$('.parallax-window').parallax({imageSrc: '/img/main__bg.png'});
