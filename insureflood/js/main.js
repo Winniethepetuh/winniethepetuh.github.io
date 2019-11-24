9ed076971f4ef9143a20b35d3ff5d54c378052b2
Winniethepetuh <55308967+Winniethepetuh@users.noreply.github.com>
2019-11-25 00:59:12 +0600

    Add files via upload

---
+++ insureflood/js/main.js
@@ -0,0 +1,52 @@
+$('.slick').slick({
+    autoplay: false,
+    autoplaySpeed: 5000,
+    fade: true,
+    arrows: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    infinite: true,
+    dots: true,
+});
+
+AOS.init();
+
+document.getElementById("slick-slide-control00").onclick = function(){
+    document.getElementById("slick-slide-control00").style.background = "#283288";
+    document.getElementById("slick-slide-control01").style.background = "rgba(0, 0, 0, 0)";
+    document.getElementById("slick-slide-control02").style.background = "rgba(0, 0, 0, 0)";
+    document.getElementById("slick-slide-control03").style.background = "rgba(0, 0, 0, 0)";
+    document.getElementById("slick-slide-control04").style.background = "rgba(0, 0, 0, 0)";
+}
+document.getElementById("slick-slide-control01").onclick = function(){
+    document.getElementById("slick-slide-control01").style.background = "#283288";
+    document.getElementById("slick-slide-control00").style.background = "rgba(0, 0, 0, 0)";
+    document.getElementById("slick-slide-control02").style.background = "rgba(0, 0, 0, 0)";
+    document.getElementById("slick-slide-control03").style.background = "rgba(0, 0, 0, 0)";
+    document.getElementById("slick-slide-control04").style.background = "rgba(0, 0, 0, 0)";
+}
+document.getElementById("slick-slide-control02").onclick = function(){
+    document.getElementById("slick-slide-control02").style.background = "#283288";
+    document.getElementById("slick-slide-control00").style.background = "rgba(0, 0, 0, 0)";
+    document.getElementById("slick-slide-control01").style.background = "rgba(0, 0, 0, 0)";
+    document.getElementById("slick-slide-control03").style.background = "rgba(0, 0, 0, 0)";
+    document.getElementById("slick-slide-control04").style.background = "rgba(0, 0, 0, 0)";
+}
+document.getElementById("slick-slide-control03").onclick = function(){
+    document.getElementById("slick-slide-control03").style.background = "#283288";
+    document.getElementById("slick-slide-control00").style.background = "rgba(0, 0, 0, 0)";
+    document.getElementById("slick-slide-control02").style.background = "rgba(0, 0, 0, 0)";
+    document.getElementById("slick-slide-control01").style.background = "rgba(0, 0, 0, 0)";
+    document.getElementById("slick-slide-control04").style.background = "rgba(0, 0, 0, 0)";
+}
+document.getElementById("slick-slide-control04").onclick = function(){
+    document.getElementById("slick-slide-control04").style.background = "#283288";
+    document.getElementById("slick-slide-control00").style.background = "rgba(0, 0, 0, 0)";
+    document.getElementById("slick-slide-control02").style.background = "rgba(0, 0, 0, 0)";
+    document.getElementById("slick-slide-control03").style.background = "rgba(0, 0, 0, 0)";
+    document.getElementById("slick-slide-control01").style.background = "rgba(0, 0, 0, 0)";
+}
+
+$(document).ready(function() {
+    $('.patient-quote').flexslider();    
+  });
